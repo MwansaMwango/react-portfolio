7f444ac6a36ae5dc8ca1894c5fe7bdeb3d322a8f
Mwansa <Mwansad.mwango@gmail.com>
2020-07-12 21:42:49 +0800

    Updates

---
+++ precache-manifest.c87573245fa4199b35d9e0605a1ca15f.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ad5d3cfadaa871bc940b67f41f91d32",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "9501df59460329fce6e4",
+    "url": "/react-portfolio/static/css/2.d0a509be.chunk.css"
+  },
+  {
+    "revision": "eb3a7a6f35aef5dedfc2",
+    "url": "/react-portfolio/static/css/main.ca67882c.chunk.css"
+  },
+  {
+    "revision": "9501df59460329fce6e4",
+    "url": "/react-portfolio/static/js/2.cd64dd86.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/react-portfolio/static/js/2.cd64dd86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb3a7a6f35aef5dedfc2",
+    "url": "/react-portfolio/static/js/main.f69e8c45.chunk.js"
+  },
+  {
+    "revision": "e4a83caef1962d3ef167",
+    "url": "/react-portfolio/static/js/runtime-main.2ce505ec.js"
+  },
+  {
+    "revision": "8c7359c36694a140b1a6b607bd38066f",
+    "url": "/react-portfolio/static/media/2.8c7359c3.jpg"
+  },
+  {
+    "revision": "5dd0c63924bcf697313d0ef747b0328f",
+    "url": "/react-portfolio/static/media/5.5dd0c639.jpg"
+  },
+  {
+    "revision": "f5ac2426a75ba316546f18a9ecb7716f",
+    "url": "/react-portfolio/static/media/8.f5ac2426.jpg"
+  },
+  {
+    "revision": "951e665a3d0fe9c1c3850e5bce106a27",
+    "url": "/react-portfolio/static/media/9.951e665a.jpg"
+  },
+  {
+    "revision": "371e0ba24523da826c025f2f69580275",
+    "url": "/react-portfolio/static/media/git.371e0ba2.png"
+  },
+  {
+    "revision": "89fdcc35aa145b75a371936597531b8e",
+    "url": "/react-portfolio/static/media/gmail.89fdcc35.png"
+  },
+  {
+    "revision": "ce82fcf1b6ed8cd8dd7ef5628282c4cf",
+    "url": "/react-portfolio/static/media/in.ce82fcf1.png"
+  },
+  {
+    "revision": "e495dd6a4715ff3b5128938e52949165",
+    "url": "/react-portfolio/static/media/jq.e495dd6a.png"
+  },
+  {
+    "revision": "f9bfbbb101ce23338869ce5ffac4ccab",
+    "url": "/react-portfolio/static/media/node.f9bfbbb1.png"
+  },
+  {
+    "revision": "665867f4ea44e5d4e23c7554ab7531b1",
+    "url": "/react-portfolio/static/media/proj1.665867f4.JPG"
+  },
+  {
+    "revision": "7265afeeb2d387f9652584a53e04f832",
+    "url": "/react-portfolio/static/media/proj2.7265afee.PNG"
+  },
+  {
+    "revision": "0bef96157d60b7973ff2e0700ccd124a",
+    "url": "/react-portfolio/static/media/proj3.0bef9615.PNG"
+  },
+  {
+    "revision": "05e2d2f5d92f5469c80a9259232a0772",
+    "url": "/react-portfolio/static/media/proj4.05e2d2f5.PNG"
+  }
+]);
